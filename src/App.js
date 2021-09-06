cb2e48a0ad6c7d6ac933e45593f24deb49547556
Ahmed Hany <dev.ahmedhany@gmail.com>
2021-09-06 18:42:46 +0200

    web-vitals

--- src/App.js
+++ src/App.js
@@ -1,25 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { Typography } from "@material-ui/core";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCYwO6x5TXpy6kg9kRyvVAHi4uz0rvGUmo",
-  authDomain: "convo-on-fire.firebaseapp.com",
-  projectId: "convo-on-fire",
-  storageBucket: "convo-on-fire.appspot.com",
-  messagingSenderId: "380612276893",
-  appId: "1:380612276893:web:b20ac86b46f4d50ad5da74",
-  measurementId: "G-KBMKBKGPQ7"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-getAnalytics(app);
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -2,6 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics,logEvent } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCYwO6x5TXpy6kg9kRyvVAHi4uz0rvGUmo",
+  authDomain: "convo-on-fire.firebaseapp.com",
+  projectId: "convo-on-fire",
+  storageBucket: "convo-on-fire.appspot.com",
+  messagingSenderId: "380612276893",
+  appId: "1:380612276893:web:b20ac86b46f4d50ad5da74",
+  measurementId: "G-KBMKBKGPQ7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,7 +31,16 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+function sendToAnalytics({ id, name, value }) {
+  logEvent(analytics,'web-vitals', {
+    eventCategory: 'Web Vitals',
+    eventAction: name,
+    eventValue: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
+    eventLabel: id, // id unique to current page load
+    nonInteraction: true, // avoids affecting bounce rate
+  });
+}
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(sendToAnalytics);
